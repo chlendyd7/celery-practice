2e18f43d99b56beb3c30128a22d3872595818eae
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-12-05 15:35:19 +0900

    feat: celery add_number() init

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -4,3 +4,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+@app.task
+def add_numbers():
+    return

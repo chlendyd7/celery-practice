d290929d695e2278d8bf56ce2d9b997d044623c9
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-12-11 10:33:32 +0900

    feat: init celery worker

--- command.md
+++ command.md
@@ -1,2 +1,3 @@
 pip freeze > requirements.txt
 docker-compose up -d --build
+docker exec -it django /bin/sh
--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 
-
 @shared_task
 def sharedtask():
-    return
+    # 작업 내용
+    return "Task Completed"

6dc71cfc4f7cf85f0123347f050ca2c95f1360a2
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-12-11 11:59:53 +0900

    feat: celery 4대를 사용하기 위해 환경 설정
    Q에 celery,celery1,celery2에 빈칸이 있으면 안됨

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -4,9 +4,14 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {
-    'newapp.tasks.task1':{'queue':'queue1'},
-    'newapp.tasks.task2':{'queue':'queue2'}
-    }
+# app.conf.task_routes = {
+#     'newapp.tasks.task1':{'queue':'queue1'},
+#     'newapp.tasks.task2':{'queue':'queue2'}
+#     }
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_strategy':'prioirty',
+}
 # 모든 모듈에 들어가서 celery task를 찾음
 app.autodiscover_tasks()

ec3e150e8bf067c348e8c707a90d6df1cbcec7e1
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-12 21:04:12 +0900

    feat: time_limit이 증가되면 celery에 error를 발생

--- dcelery/dcelery/celery_tasks/ex7_task_timeouts_revoking.py
+++ dcelery/dcelery/celery_tasks/ex7_task_timeouts_revoking.py
@@ -1,37 +1,13 @@
+import time
 from celery import chain, group
 from dcelery.celery_config import app
 import traceback
 '''
-    from dcelery.celery_tasks.ex6_dead_letter_queu import run_task_group
-    handle_failed_task.delay()
+    from dcelery.celery_tasks.ex7_task_timeouts_revoking import long_running_task
+    long_running_task()
 '''
-# task가 실패 시 완료 될 때 까지 대기열에 남아있음 안정성 증가
-app.conf.task_acks_late = True
 
-# worker 손실 시 task를 거부
-app.conf.task_reject_on_worker_lost = True
-
-@app.task(queue='tasks')
-def my_task(z):
-    try:
-        if z == 2:
-            raise ValueError("Error wrong number")
-    except Exception as e:
-        traceback_str = traceback.format_exc() 
-        task_id = my_task.request.id
-        handle_failed_task.apply_async(args=(z, str(e),traceback_str, task_id))
-        raise
-
-@app.task(queue='dead_letter')
-def handle_failed_task(z, task_id, exception, traceback_str):
-    print(f"Task failed: task_id={task_id}, z={z}, exception={exception}")
-    print(traceback_str)
-    return "Custom logic to process"
-
-def run_task_group():
-    task_group = group(
-        my_task.s(1),
-        my_task.s(2),
-        my_task.s(3),
-    )
-    task_group.apply_async()
+@app.task(queue='tasks', time_limit=5)
+def long_running_task():
+    time.sleep(6)
+    return "Task completed successfully"

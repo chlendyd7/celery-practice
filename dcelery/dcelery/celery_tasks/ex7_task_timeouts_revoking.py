70089004e6ae31b6106936a3bcd141f98c8fa553
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-12 21:08:07 +0900

    feat: timeout을 수동으로 설정함

--- dcelery/dcelery/celery_tasks/ex7_task_timeouts_revoking.py
+++ dcelery/dcelery/celery_tasks/ex7_task_timeouts_revoking.py
@@ -7,7 +7,14 @@ import traceback
     long_running_task()
 '''
 
-@app.task(queue='tasks', time_limit=5)
+@app.task(queue='tasks', time_limit=10)
 def long_running_task():
     time.sleep(6)
     return "Task completed successfully"
+
+def execute_task_exaples():
+    result = long_running_task.delay()
+    try:
+        task_result = result.get(timeout=4)
+    except TimeoutError:
+        print("Task timed out")

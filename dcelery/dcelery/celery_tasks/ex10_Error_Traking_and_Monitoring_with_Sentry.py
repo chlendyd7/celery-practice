79e0c1d9dd00ac55e6e3b814a90bf4db7d762c41
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-15 18:03:10 +0900

    feat: ex10) django 실행 명령어 등록

--- dcelery/dcelery/celery_tasks/ex10_Error_Traking_and_Monitoring_with_Sentry.py
+++ dcelery/dcelery/celery_tasks/ex10_Error_Traking_and_Monitoring_with_Sentry.py
@@ -1,7 +1,9 @@
 from sentry_sdk import capture_exception
-
 from dcelery.celery_config import app
-
+'''
+from dcelery.celery_tasks.ex10_Error_Traking_and_Monitoring_with_Sentry import divide_numbers
+divide_numbers.delay(10, 0)
+'''
 @app.task(queue="tasks")
 def divide_numbers(a,b):
     try:

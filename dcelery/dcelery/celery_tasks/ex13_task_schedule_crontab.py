84623454c92b5b6dd720d8f4862e1c210ab6de7c
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-16 22:08:08 +0900

    feat: celery_beat를 사용해서 admin에서 db 테이블을 통해 쉽게 저장 할 수 있음

--- dcelery/dcelery/celery_tasks/ex13_task_schedule_crontab.py
+++ dcelery/dcelery/celery_tasks/ex13_task_schedule_crontab.py
@@ -2,22 +2,22 @@ from datetime import timedelta
 from dcelery.celery_config import app
 from celery.schedules import crontab
 
-app.conf.beat_schedule = {
-    'task1':{
-        'task': 'dcelery.celery_tasks.ex13_task_schedule_crontab.task1',
-        'schedule': crontab(minute='0-59/10', hour='0-5', day_of_week='mon'),
-        'kwargs': {'foo':'bar'},
-        'args': {1,2},
-        'options': {
-            'queue':'tasks',
-            'priory':5,
-        }
-    },
-    'task2':{
-        'task': 'dcelery.celery_tasks.ex13_task_schedule_crontab.task2',
-        'schedule': timedelta(seconds=10),
-    },
-}
+# app.conf.beat_schedule = {
+#     'task1':{
+#         'task': 'dcelery.celery_tasks.ex13_task_schedule_crontab.task1',
+#         'schedule': crontab(minute='0-59/10', hour='0-5', day_of_week='mon'),
+#         'kwargs': {'foo':'bar'},
+#         'args': {1,2},
+#         'options': {
+#             'queue':'tasks',
+#             'priory':5,
+#         }
+#     },
+#     # 'task2':{
+#     #     'task': 'dcelery.celery_tasks.ex13_task_schedule_crontab.task2',
+#     #     'schedule': timedelta(seconds=10),
+#     # },
+# }
 
 @app.task(queue='tasks')
 def task1(a, b, **kwargs):
--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'newapp',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "amqp://guest:guest@rabbitmq:5672/")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
